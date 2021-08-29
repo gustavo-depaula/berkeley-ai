26a7d2e144e17dc27de508e360171720be7b36e0
Gustavo de Paula <gustavodepaula@disroot.org>
2021-08-29 13:04:57 -0300

    (p1 search) question 2

--- search/search.py
+++ search/search.py
@@ -132,8 +132,7 @@ def depthFirstSearch(problem):
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return genericProblemSearch(problem, util.Queue)
 
 
 def uniformCostSearch(problem):
